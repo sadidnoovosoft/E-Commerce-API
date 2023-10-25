25d7570bf3edc5c863b9cdd93be15b18f93d4aea
sadidnoovosoft <sadid.shaikh@noovosoft.com>
2023-10-25 11:14:58 +0530

    Implement UserDetailsService and necessary beans for security

--- src/main/kotlin/com/example/ecommerceapi/repository/UserRepository.kt
+++ src/main/kotlin/com/example/ecommerceapi/repository/UserRepository.kt
@@ -3,4 +3,6 @@ package com.example.ecommerceapi.repository
 import com.example.ecommerceapi.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long> {
+    fun findUserByEmail(email: String): User
+}
